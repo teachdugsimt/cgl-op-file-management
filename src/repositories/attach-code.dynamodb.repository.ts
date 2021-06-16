903217d46d42e2fc58166f3b088f25e542801f25
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-06-17 00:47:46 +0700

    chore: function query

--- src/repositories/attach-code.dynamodb.repository.ts
+++ src/repositories/attach-code.dynamodb.repository.ts
@@ -82,8 +82,45 @@ export default class AttachCodeRepository {
     return Item && Object.keys(Item)?.length ? Item : null
   }
 
+  async queryWithFilenameBegin(): Promise<any> {
+    const prefixString = "tes"
+    const params = {
+      TableName: "cgl_attach_code",
+      FilterExpression: 'begins_with(#user_id, :user_id)',
+      ExpressionAttributeValues: {
+        ':user_id': prefixString
+      },
+      ExpressionAttributeNames: {
+        '#user_id': "user_id"
+      },
+    };
+    return await documentClient.scan(params).promise();
+
+    // NOTE : Query => must specify primary key
+    // const params: DocumentClient.QueryInput = {
+    //   TableName: "cgl_attach_code",
+    //   KeyConditionExpression: 'begins_with(#file_name, :file_name)',
+    //   ExpressionAttributeNames: {
+    //     // "#user_id": "user_id",
+    //     "#file_name": 'file_name'
+    //   },
+    //   ExpressionAttributeValues: {
+    //     // ":user_id": "1234",
+    //     ":file_name": "artist88"
+    //   }
+    // };
+    // return await documentClient.query(params).promise();
+
+  }
+
 }
 
+// const main = async () => {
+//   const repo = new AttachCodeRepository()
+//   const res = await repo.queryWithFilenameBegin()
+//   console.log("Result : ", res)
+// }
+// main()
 
 // const data = [
 //   {
